77c632578f709a52a068205c369029376b36eefc
Thanabordee Nammungkhun <49986131+ThanabordeeN@users.noreply.github.com>
2025-08-10 12:42:19 +0700

    Document property inspector text editing

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,7 @@ const App: React.FC = () => {
         <SandboxIframe
           htmlContent={htmlContent}
           onElementSelect={handleElementSelect}
+          onHtmlChange={setHtmlContent}
         />
       </div>
       <div className="w-1/3">
